3eed6637deae698094e9baeb4a3bb729b3b1c8ac
Emily <fabiana29vipar@gmail.com>
2025-11-13 10:36:50 -0500

    Se hicieron cambios

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // 👇 Ajuste importante para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: 'process.env.NODE_ENV === 'production' ? '/gametracker-frontend/' : '/'', 
   server: {
     port: 3000,
     proxy: {
